7213b516fcafd7bc821b1e8c1840a87e3493674a
Greg Cannon <gregory.cannon@originate.com>
2017-08-18 15:04:42 -0700

    fix poll screen

--- client/index.js
+++ client/index.js
@@ -5,18 +5,22 @@ import createHistory from 'history/createBrowserHistory';
 
 import Homepage from './components/index';
 import Poll from './components/poll';
-import App from './components/app.js';
+import App from './components/app';
 
 // This file is the client entry point, specified in webpack.config
 
-const AppClient = (props) => (
-  <Router history={createHistory()}>
-    <App>
-      <Route exact path="/" component={Homepage}/>
-      <Route path="/poll" component={Poll}/>
-    </App>
-  </Router>
-)
+const AppClient = (props) => {
+  console.log(Poll);
+  console.log(App);
+  return (
+    <Router history={createHistory()}>
+      <App>
+        <Route exact path="/" component={Homepage}/>
+        <Route path="/poll" component={Poll}/>
+      </App>
+    </Router>
+  );
+}
 
 ReactDOM.render(
   <AppClient />,
